19eb70f82e7a527d95526e3f48c74f2036eec700
makavelka <vovchilnik@yandex.ru>
2015-06-02 17:47:18 +0300

    add swipe refresh

--- app/src/main/java/ru/macrobit/abonnews/ui/fragment/NewsFragment.java
+++ app/src/main/java/ru/macrobit/abonnews/ui/fragment/NewsFragment.java
@@ -1,12 +1,12 @@
 package ru.macrobit.abonnews.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -22,10 +22,11 @@ import ru.macrobit.abonnews.model.News;
 import ru.macrobit.abonnews.model.ShortNews;
 
 
-public class NewsFragment extends EnvFragment implements OnTaskCompleted{
+public class NewsFragment extends EnvFragment implements OnTaskCompleted, SwipeRefreshLayout.OnRefreshListener {
 
     ListView mListView;
     News[] mNews;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,14 +34,18 @@ public class NewsFragment extends EnvFragment implements OnTaskCompleted{
         if (container == null) {
             return null;
         }
-        View view = (RelativeLayout) inflater.inflate(R.layout.fragment_newslist,
+        View view = inflater.inflate(R.layout.fragment_newslist,
                 container, false);
+
         mListView = (ListView) view.findViewById(R.id.listView);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh);
         getNewsFromServer();
         return view;
     }
 
-    private void listViewInit(ArrayList<ShortNews> newsList) {
+    private void UiInit(ArrayList<ShortNews> newsList) {
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeColors(android.R.color.holo_blue_light);
         NewsAdapter adapter = new NewsAdapter(getActivity(), R.layout.news_item, newsList);
         mListView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
@@ -70,6 +75,13 @@ public class NewsFragment extends EnvFragment implements OnTaskCompleted{
     public void onTaskCompleted(String result) {
         mNews = GsonUtils.fromJson(result, News[].class);
         ArrayList<ShortNews> newsList = NewsUtils.generateShortNews(mNews);
-        listViewInit(newsList);
+        UiInit(newsList);
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        getNewsFromServer();
     }
 }
