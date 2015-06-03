1c295e6cd95bf62bf346415f3d76527810ef5b4d
makavelka <vovchilnik@yandex.ru>
2015-06-03 13:20:07 +0300

    add  comments listview

--- app/src/main/java/ru/macrobit/abonnews/ui/fragment/DetailNewsFragment.java
+++ app/src/main/java/ru/macrobit/abonnews/ui/fragment/DetailNewsFragment.java
@@ -6,20 +6,31 @@ import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ru.macrobit.abonnews.OnTaskCompleted;
 import ru.macrobit.abonnews.R;
+import ru.macrobit.abonnews.Values;
+import ru.macrobit.abonnews.adapter.CommentsAdapter;
+import ru.macrobit.abonnews.controller.GsonUtils;
 import ru.macrobit.abonnews.controller.ImageUtils;
+import ru.macrobit.abonnews.loader.GetRequest;
+import ru.macrobit.abonnews.model.Comments;
 import ru.macrobit.abonnews.model.FullNews;
 
 
-public class DetailNewsFragment extends EnvFragment {
+public class DetailNewsFragment extends EnvFragment implements OnTaskCompleted{
 
     TextView mTitle;
     TextView mDate;
     TextView mBody;
     ImageView mImage;
+    ExpandableListView mListView;
 
 
     @Override
@@ -36,21 +47,38 @@ public class DetailNewsFragment extends EnvFragment {
 
     private void initFragment(View parent) {
         Bundle bundle = this.getArguments();
-        mTitle = (TextView) parent.findViewById(R.id.title);
-        mDate = (TextView) parent.findViewById(R.id.date);
-        mBody = (TextView) parent.findViewById(R.id.body);
-        mImage = (ImageView) parent.findViewById(R.id.imageView);
+        mTitle = (TextView) parent.findViewById(R.id.det_title);
+        mDate = (TextView) parent.findViewById(R.id.det_date);
+        mBody = (TextView) parent.findViewById(R.id.det_body);
+        mImage = (ImageView) parent.findViewById(R.id.det_imageView);
+        mListView = (ExpandableListView) parent.findViewById(R.id.det_listView);
         FullNews news = bundle.getParcelable("data");
         mTitle.setText(news.getTitle());
         mDate.setText(news.getDate());
         Spanned spanned = Html.fromHtml(news.getBody());
         mBody.setText(spanned);
+        getComments(news.getId() + "/comments/");
         ImageUtils.getUIL(getActivity()).displayImage(news.getImageUrl(), mImage);
+    }
+
+    private void getComments(String url) {
+        new GetRequest(DetailNewsFragment.this).execute(Values.GET_POST + url);
     }
 
     @Override
     public void onCreate(Bundle arg0) {
         super.onCreate(arg0);
+    }
+
+    @Override
+    public void onTaskCompleted(String result) {
+        Comments[] comments = GsonUtils.fromJson(result, Comments[].class);
+        ArrayList<Comments> arrayList = new ArrayList<Comments>(Arrays.asList(comments));
+        ArrayList<ArrayList<Comments>> group = new ArrayList<ArrayList<Comments>>();
+        group.add(arrayList);
+        CommentsAdapter adapter = new CommentsAdapter(getActivity(), group);
+        mListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
 
