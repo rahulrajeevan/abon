8354a83c9b1bec442417dd4b85745074c089bc6a
makavelka <vovchilnik@yandex.ru>
2015-06-03 13:18:48 +0300

    add header to listview

--- app/src/main/java/ru/macrobit/abonnews/adapter/NewsAdapter.java
+++ app/src/main/java/ru/macrobit/abonnews/adapter/NewsAdapter.java
@@ -1,4 +1,4 @@
-package ru.macrobit.abonnews;
+package ru.macrobit.abonnews.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import ru.macrobit.abonnews.R;
 import ru.macrobit.abonnews.controller.ImageUtils;
 import ru.macrobit.abonnews.model.ShortNews;
 
@@ -51,10 +52,14 @@ public class NewsAdapter extends ArrayAdapter<ShortNews> {
         if (convertView == null) {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(mContext);
-            convertView = inflater.inflate(R.layout.news_item, parent, false);
+            if (position != 0) {
+                convertView = inflater.inflate(R.layout.news_item, parent, false);
+            } else {
+                convertView = inflater.inflate(R.layout.main_news_item, parent, false);
+            }
             viewHolder.title = ((TextView) convertView.findViewById(R.id.textBody));
-            viewHolder.date = ((TextView) convertView.findViewById(R.id.date));
-            viewHolder.image = ((ImageView) convertView.findViewById(R.id.imageView));
+            viewHolder.date = ((TextView) convertView.findViewById(R.id.det_date));
+            viewHolder.image = ((ImageView) convertView.findViewById(R.id.det_imageView));
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
