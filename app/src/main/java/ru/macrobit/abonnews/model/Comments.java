9fbbac20e8356a938791674ed1564755c582e4c4
makavelka <vovchilnik@yandex.ru>
2015-06-03 13:20:38 +0300

    create model for comments

---
+++ app/src/main/java/ru/macrobit/abonnews/model/Comments.java
@@ -0,0 +1,111 @@
+package ru.macrobit.abonnews.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Comp on 03.06.2015.
+ */
+public class Comments {
+    @SerializedName("ID")
+    private int id;
+    private int post;
+    private String content;
+    private String status;
+    private String type;
+    private int parent;
+    private Author author;
+    private String date;
+    @SerializedName("date_tz")
+    private String dateTz;
+    @SerializedName("date_gmt")
+    private String dateGmt;
+    private Meta meta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPost() {
+        return post;
+    }
+
+    public void setPost(int post) {
+        this.post = post;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDateTz() {
+        return dateTz;
+    }
+
+    public void setDateTz(String dateTz) {
+        this.dateTz = dateTz;
+    }
+
+    public String getDateGmt() {
+        return dateGmt;
+    }
+
+    public void setDateGmt(String dateGmt) {
+        this.dateGmt = dateGmt;
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+}
