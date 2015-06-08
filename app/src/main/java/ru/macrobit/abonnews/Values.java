ee18794d2d9649ddc7a7273747e67df3d6b48175
makavelka <vovchilnik@yandex.ru>
2015-06-08 17:59:23 +0300

    fix get all posts, add search url

--- app/src/main/java/ru/macrobit/abonnews/Values.java
+++ app/src/main/java/ru/macrobit/abonnews/Values.java
@@ -2,9 +2,10 @@ package ru.macrobit.abonnews;
 
 public class Values {
     public static final String URL = "http://abon-news.ru/";
-    public static final String GET_POST = URL + "wp-json/posts/";
+    public static final String GET_POST = URL + "wp-json/posts?filter[posts_per_page]=-1";
     public static final String AUTORIZATION = URL + "wp-login.php";
     public static final String MEDIA = URL + "media";
     public static final String COMMENTS = "/comments";
     public static final String PUSH = URL + "/push";
+    public static final String SEARCH = URL + "wp-json/posts?filter[s]=";
 }
