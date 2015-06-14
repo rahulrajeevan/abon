69b5c03714fef99fd6d04d6469424b21411657a1
makavelka <vovchilnik@yandex.ru>
2015-06-15 00:01:48 +0300

    add methods for saving cookies

--- app/src/main/java/ru/macrobit/abonnews/controller/Utils.java
+++ app/src/main/java/ru/macrobit/abonnews/controller/Utils.java
@@ -3,11 +3,19 @@ package ru.macrobit.abonnews.controller;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import org.apache.http.client.CookieStore;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
+
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import ru.macrobit.abonnews.Values;
+import ru.macrobit.abonnews.model.ShortCookie;
 
 /**
  * Created by Ghost Surfer on 14.06.2015.
@@ -45,4 +53,27 @@ public class Utils {
         return context.getSharedPreferences(Values.PREF, Context.MODE_PRIVATE);
     }
 
+    public static void saveCookieToSharedPreferences(String key, CookieStore cookieStore,
+                                                          SharedPreferences pref) {
+        List<Cookie> list = cookieStore.getCookies();
+        Set<ShortCookie> cookies = new HashSet<>();
+        for (Cookie c:list) {
+            cookies.add(new ShortCookie(c.getName(), c.getValue()));
+        }
+        SharedPreferences.Editor edit = pref.edit();
+        String s = GsonUtils.toJson(cookies);
+        edit.putString(key, s);
+        edit.commit();
+    }
+
+    public static BasicClientCookie[] loadCookieFromSharedPreferences(String key,
+                                                     SharedPreferences pref) {
+        String s = pref.getString(key, null);
+        ShortCookie[] cookies = GsonUtils.fromJson(s, ShortCookie.class);
+        BasicClientCookie[] basicClientCookies = new BasicClientCookie[cookies.length];
+        for (int i = 0; i<cookies.length; i++) {
+            basicClientCookies[i] = new BasicClientCookie(cookies[i].getKey(), cookies[i].getValue());
+        }
+        return basicClientCookies;
+    }
 }
