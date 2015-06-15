3a6679888a861bbf1d5128905f4a9a75e2696422
makavelka <vovchilnik@yandex.ru>
2015-06-15 17:34:10 +0300

    add needed fields

--- app/src/main/java/ru/macrobit/abonnews/model/ShortCookie.java
+++ app/src/main/java/ru/macrobit/abonnews/model/ShortCookie.java
@@ -1,15 +1,42 @@
 package ru.macrobit.abonnews.model;
 
+import java.util.Date;
+
 /**
  * Created by Ghost Surfer on 14.06.2015.
  */
 public class ShortCookie {
     private String key;
     private String value;
+    private String domain;
+    private Date expiryDate;
 
     public ShortCookie(String key, String value) {
         this.key = key;
         this.value = value;
+    }
+
+    public ShortCookie(String key, String value, String domain, Date expiryDate) {
+        this.key = key;
+        this.value = value;
+        this.domain = domain;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
     }
 
     public String getKey() {
