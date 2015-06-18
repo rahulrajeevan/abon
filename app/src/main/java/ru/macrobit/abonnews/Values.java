348b57d8cf362cf1cbf789b208c7a23b5f115b2d
makavelka <vovchilnik@yandex.ru>
2015-06-18 17:39:35 +0300

    add new values

--- app/src/main/java/ru/macrobit/abonnews/Values.java
+++ app/src/main/java/ru/macrobit/abonnews/Values.java
@@ -3,7 +3,7 @@ package ru.macrobit.abonnews;
 public class Values {
     public static final String URL = "http://abon-news.ru/";
     public static final String GET_POST = URL + "wp-json/posts?filter[posts_per_page]=-1";
-    public static final String AUTORIZATION = URL + "wp-login.php";
+    public static final String AUTHORIZATION = URL + "wp-login.php";
     public static final String MEDIA = URL + "wp-json/media";
     public static final String COMMENTS = "/comments";
     public static final String PUSH = URL + "/push";
@@ -15,6 +15,13 @@ public class Values {
     public static final String PROFILE = URL + "wp-json/users/me";
     public static final int REQUEST_ULOGIN = 1;
     public static final String TOKEN = "token";
-    public static final String ULOGIN = URL + "?ulogin=";
-    public static final String SOC_AUTORIZATION = "&backurl=http://abon-news.ru/wp-login.php?loggedout=true";
+    public static final String ULOGIN = URL + "?ulogin=token&backurl=http://abon-news.ru/wp-login.php?loggedout=true";
+//    public static final String SOC_AUTORIZATION = URL + "?ulogin=token&backurl=http://abon-news.ru/wp-login.php";
+    public static final String SOC_AUTORIZATION = "http://abon-news.ru/?ulogin=token&backurl=http%3A%2F%2Fabon-news.ru%2Fwp-login.php%3Fredirect_to%3Dhttp%253A%252F%252Fabon-news.ru%252Fwp-admin%252F%26reauth%3D1";
+//            "?loggedout=true";
+    public static final String DETAIL_TAG = "detail";
+
+    public static final String AUTHORIZATION_TAG = "authorization";
+    public static final String NEWS_TAG = "news";
+    public static final String PROFILE_TAG = "profile";
 }
