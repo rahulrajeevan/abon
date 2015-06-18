c918d4ae741d6a2b5f982ef281cb6982189b585c
makavelka <vovchilnik@yandex.ru>
2015-06-19 00:07:27 +0300

    add getmedia method

--- app/src/main/java/ru/macrobit/abonnews/controller/NewsUtils.java
+++ app/src/main/java/ru/macrobit/abonnews/controller/NewsUtils.java
@@ -1,5 +1,7 @@
 package ru.macrobit.abonnews.controller;
 
+import android.content.Context;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -7,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import ru.macrobit.abonnews.loader.GetRequest;
+import ru.macrobit.abonnews.model.Media;
 import ru.macrobit.abonnews.model.News;
 import ru.macrobit.abonnews.model.ShortNews;
 
@@ -17,14 +21,14 @@ public class NewsUtils {
 
     public static ArrayList<ShortNews> generateShortNews(News[] news) {
         ArrayList<ShortNews> arrayList = new ArrayList<ShortNews>();
-        for (News n:news) {
+        for (News n : news) {
             ShortNews shortNews = new ShortNews(n.getTitle(), parseDate(n.getDate()), n.getFeaturedImage().getGuid(), String.valueOf(n.getId()), n.isSticky());
             arrayList.add(shortNews);
         }
         return arrayList;
     }
 
-    private static String parseDate (String s) {
+    private static String parseDate(String s) {
         s = s.replaceAll("T", " ");
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
         Date date = null;
@@ -35,5 +39,18 @@ public class NewsUtils {
         }
         DateFormat df = new SimpleDateFormat("MM.dd.yyyy HH:mm:ss");
         return df.format(date);
+    }
+
+    public static ArrayList<Media> getMediaById(String id, Media[] media) {
+        int i = Integer.parseInt(id);
+        ArrayList<Media> array = new ArrayList<>();
+        for (Media m : media) {
+            if (m.getParent() != null) {
+                if (m.getParent().getId() == i) {
+                    array.add(m);
+                }
+            }
+        }
+        return array;
     }
 }
