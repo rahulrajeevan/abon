4e7aec840bda2e8b680899368b28b042324c99dd
makavelka <vovchilnik@yandex.ru>
2015-06-19 17:37:53 +0300

    add new values

--- app/src/main/java/ru/macrobit/abonnews/Values.java
+++ app/src/main/java/ru/macrobit/abonnews/Values.java
@@ -4,6 +4,7 @@ public class Values {
     public static final String URL = "http://abon-news.ru/";
     public static final String GET_POST = URL + "wp-json/posts?filter[posts_per_page]=-1";
     public static final String AUTHORIZATION = URL + "wp-login.php";
+    public static final String POSTS = URL + "wp-json/posts/";
     public static final String MEDIA = URL + "wp-json/media?filter[posts_per_page]=-1";
     public static final String COMMENTS = "/comments";
     public static final String PUSH = URL + "/push";
