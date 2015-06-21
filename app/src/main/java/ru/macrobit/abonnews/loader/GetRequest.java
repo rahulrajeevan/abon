767ae64b38e920e4673353d007f362a7893f41da
makavelka <vovchilnik@yandex.ru>
2015-06-22 00:50:58 +0300

    fix

--- app/src/main/java/ru/macrobit/abonnews/loader/GetRequest.java
+++ app/src/main/java/ru/macrobit/abonnews/loader/GetRequest.java
@@ -5,27 +5,19 @@ package ru.macrobit.abonnews.loader;
  */
 
 import android.os.AsyncTask;
-import android.webkit.CookieManager;
 
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import ru.macrobit.abonnews.OnTaskCompleted;
 
@@ -55,7 +47,6 @@ public class GetRequest extends AsyncTask<String, String, String> {
             URL url = new URL(urls[0]);
             HttpGet httpGet = new HttpGet(url.toString());
             DefaultHttpClient client = new DefaultHttpClient();
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
             if (mCookies != null) {
                 client.setCookieStore(mCookies);
             }
