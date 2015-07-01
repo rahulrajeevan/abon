ea5202c629f72c696a3a7430198811bf5a6eb3a1
makavelka <vovchilnik@yandex.ru>
2015-07-01 17:38:57 +0300

    add version

--- app/src/main/java/ru/macrobit/abonnews/ui/fragment/AboutFragment.java
+++ app/src/main/java/ru/macrobit/abonnews/ui/fragment/AboutFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import ru.macrobit.abonnews.BuildConfig;
 import ru.macrobit.abonnews.R;
 
 public class AboutFragment extends EnvFragment {
@@ -17,6 +19,9 @@ public class AboutFragment extends EnvFragment {
         }
         View view = inflater.inflate(R.layout.fragment_about,
                 container, false);
+        String versionName = BuildConfig.VERSION_NAME;
+        TextView tv = (TextView) view.findViewById(R.id.version);
+        tv.setText(getString(R.string.version) + ": " + versionName);
         return view;
     }
 }
