62fa4c17bebfc587bb8bbfd7ab322013e2f9fade
makavelka <vovchilnik@yandex.ru>
2015-07-09 11:32:43 +0300

    add ACRA

---
+++ app/src/main/java/ru/macrobit/abonnews/AbonApplication.java
@@ -0,0 +1,20 @@
+package ru.macrobit.abonnews;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(formKey="",
+        formUri = "http://abon-news.ru/test/test.php",
+        mode = ReportingInteractionMode.TOAST,
+        forceCloseDialogAfterToast = true, // optional, default false
+        resToastText = R.string.crash_toast_text)
+public class AbonApplication extends Application {
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+    }
+}
