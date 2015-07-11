be5db4ca8c4d1a7d99d37f75f017d96f9b152cd6
makavelka <vovchilnik@yandex.ru>
2015-07-11 17:15:27 +0300

    fix picture loading

--- app/src/main/java/ru/macrobit/abonnews/loader/AddMediaRequest.java
+++ app/src/main/java/ru/macrobit/abonnews/loader/AddMediaRequest.java
@@ -2,23 +2,16 @@ package ru.macrobit.abonnews.loader;
 
 import android.os.AsyncTask;
 
-import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -49,13 +42,7 @@ public class AddMediaRequest extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... urls) {
         try {
-            HttpParams httpParams = new BasicHttpParams();
-            HttpProtocolParams.setVersion(httpParams,
-                    HttpVersion.HTTP_1_1);
-            HttpProtocolParams.setContentCharset(httpParams, "UTF-8");
-            HttpProtocolParams.setHttpElementCharset(httpParams,
-                    "UTF-8");
-            DefaultHttpClient client = new DefaultHttpClient(httpParams);
+            DefaultHttpClient client = new DefaultHttpClient();
             if (mCookies != null) {
                 client.setCookieStore(mCookies);
             }
@@ -63,18 +50,11 @@ public class AddMediaRequest extends AsyncTask<String, String, String> {
             builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
             File f = new File(mFile);
             if (f.exists()) {
-                builder.addPart("file", new FileBody(f, ContentType.create("image/jpeg"), f.getName()));
+                builder.addPart("file", new FileBody(f));
 //                builder.addPart(part);
             }
             HttpPost post = new HttpPost(urls[0]);
-            int portOfProxy = android.net.Proxy.getDefaultPort();
-            if (portOfProxy > 0) {
-                HttpHost proxy = new HttpHost(
-                        android.net.Proxy.getDefaultHost(), portOfProxy);
-                client.getParams().setParameter(
-                        ConnRoutePNames.DEFAULT_PROXY, proxy);
-            }
-            post.setHeader("Content-type", "multipart/form-data");
+//            post.setHeader("Content-type", "multipart/form-data");
             post.setEntity(builder.build());
             HttpResponse response = client.execute(post);
             BufferedReader reader = new BufferedReader(
