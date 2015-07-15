ef07e4a3820e5718bd4b7c334a4ac9e5febcf452
makavelka <vovchilnik@yandex.ru>
2015-07-16 00:08:24 +0300

    new model for push response

---
+++ app/src/main/java/ru/macrobit/abonnews/model/PushIncome.java
@@ -0,0 +1,25 @@
+package ru.macrobit.abonnews.model;
+
+/**
+ * Created by Ghost Surfer on 15.07.2015.
+ */
+public class PushIncome {
+    private String postid;
+    private String text;
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
