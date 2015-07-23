eab127058b9d68e4204cd670daa8bfe3eab90e15
makavelka <vovchilnik@yandex.ru>
2015-07-23 15:55:09 +0300

    add approved field

--- app/src/main/java/ru/macrobit/abonnews/model/AddComment.java
+++ app/src/main/java/ru/macrobit/abonnews/model/AddComment.java
@@ -9,14 +9,24 @@ public class AddComment {
     private String content;
     @SerializedName("parent_id")
     private int parentId;
+    private int approved;
 
     public AddComment(String content, int id) {
         this.content = content;
         parentId = id;
     }
 
+    public int getApproved() {
+        return approved;
+    }
+
+    public void setApproved(int approved) {
+        this.approved = approved;
+    }
+
     public AddComment(String content) {
         this.content = content;
+        this.approved = 1;
     }
 
     public String getContent() {
